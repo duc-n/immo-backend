c63bb5b9d09d9a7b80eba66470ed0889791e6aba
duc <duc.nguyen80@gmail.com>
2019-10-20 23:27:09 +0200

    Spring cloud integration

---
+++ README.md
@@ -0,0 +1,15 @@
+
+## Project Structure
+
+* ci-vagrant
+ * from immo-backend
+    ./mvnw clean install
+    
+ * execute docker-compose up -d
+ 
+ http://localhost:8761/
+ 
+ docker-compose down
+ docker-compose up -d
+ 
+ 
---
+++ immo-service/src/main/java/com/cele/immo/ImmoApplication.java
@@ -0,0 +1,47 @@
+package com.cele.immo;
+
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
+import org.springframework.context.annotation.Bean;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
+
+@SpringBootApplication
+@EnableEurekaClient
+public class ImmoApplication {
+    private Logger log = LoggerFactory.getLogger(this.getClass());
+
+    //in case you want to encrypt password
+    @Bean
+    public BCryptPasswordEncoder passwordEncoder() {
+        return new BCryptPasswordEncoder();
+    }
+
+
+    public static void main(String[] args) {
+        SpringApplication.run(ImmoApplication.class, args);
+    }
+
+
+   /* @Bean
+    CommandLineRunner start(UserAccountRepository userAccountRepository, PasswordEncoder passwordEncoder) {
+        return args -> {
+            List<Role> roles = new ArrayList<>();
+            Role role = new Role();
+            role.setId("ADMIN");
+            roles.add(role);
+
+
+            UserAccount user = UserAccount.builder().username("Duc")
+                    .firstName("Duc").lastName("NGUYEN").email("duc@gmail.com")
+                    .password(passwordEncoder.encode("password"))
+                    .roles(roles).build();
+            userAccountRepository.save(user).subscribe();
+
+            userAccountRepository.findAll().log().subscribe(u -> log.info("user: {}", u));
+        };
+    }*/
+}
+
